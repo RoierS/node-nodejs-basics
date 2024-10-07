8581c0e299e94caf2952beb939a0e2e7307ccda0
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-10-07 12:56:56 +0200

    feat: implement read fn

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
+  try {
+    const fileContent = await fs.readFile(filePath, "utf-8");
+
+    console.log(`\x1b[42m File content:\x1b[0m`, fileContent);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw error;
+  }
 };
 
 await read();
