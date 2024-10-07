02f748897e9cfb1fbd4e7a022703a392e5bdecc6
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-10-07 17:51:45 +0200

    feat: implement calcHash fn

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import path from "path";
+import crypto from "crypto";
+import { fileURLToPath } from "url";
+import { createReadStream } from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  const hash = crypto.createHash("sha256");
+  const fileStream = createReadStream(filePath);
+
+  fileStream.on("data", (chunk) => hash.update(chunk));
+
+  fileStream.on("end", () =>
+    console.log(`\x1b[42m Hash:\x1b[0m`, hash.digest("hex")),
+  );
+
+  fileStream.on("error", (error) => {
+    throw new Error(error);
+  });
 };
 
 await calculateHash();
