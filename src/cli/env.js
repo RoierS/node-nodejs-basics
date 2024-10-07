483665dbc27c1b5996bb390e9855ce5b13bae1e7
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-10-07 13:13:46 +0200

    feat: create fn parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const RSS_ENV_VARIABLES = Object.entries(process.env)
+    .filter(([envVarName]) => envVarName.startsWith("RSS_"))
+    .map(([envVarName, envVarValue]) => `${envVarName}=${envVarValue}`)
+    .join("; ");
+
+  console.log(`\x1b[42m RSS_ENV_VARIABLES: \x1b[0m`);
+  console.log(RSS_ENV_VARIABLES);
 };
 
 parseEnv();
