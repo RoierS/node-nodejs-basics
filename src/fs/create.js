8fd232e4f34abb3e8161ee918227f4de10977590
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-10-07 10:38:04 +0200

    feat: implement function that creates new fresh.txt file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+
+  try {
+    await fs.writeFile(filePath, "I am fresh and young", { flag: "wx" });
+  } catch (error) {
+    if (error.code === "EEXIST") throw new Error("FS operation failed");
+    throw error;
+  }
 };
 
 await create();
