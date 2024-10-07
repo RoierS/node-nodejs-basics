32e3c79efc1899f209a75f0674888209b77202e3
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-10-07 19:54:53 +0200

    feat: implement transform fn

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import { pipeline } from "stream/promises";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const readableStream = process.stdin;
+  const writableStream = process.stdout;
+
+  const transformStream = new Transform({
+    transform(chunk, _, cb) {
+      const reversedChunk = chunk
+        .toString()
+        .trim()
+        .split("")
+        .reverse()
+        .join("");
+
+      cb(null, `${reversedChunk} \n`);
+    },
+  });
+
+  try {
+    pipeline(readableStream, transformStream, writableStream);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await transform();
