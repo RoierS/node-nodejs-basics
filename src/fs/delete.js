49f19e7d878c3c5bb185af871311dae7ba854e4f
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-10-07 12:13:46 +0200

    feat: implement delete fn

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.join(__dirname, "files", "fileToRemove.txt");
+
+  try {
+    await fs.unlink(filePath);
+
+    console.log(`\x1b[42m File deleted successfully! \x1b[0m`);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw error;
+  }
 };
 
 await remove();
